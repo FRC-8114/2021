c31ccd78b22bbd153bf590b8cc8daf82a39d53a9
Jack <doesnotcomputeeeee@gmail.com>
2021-02-06 10:19:43 -0600

    Setup Constants.java file.
    Replaced default values with those derived from robot characterization
    (as described in trajectories tutorial) and updated
    kWheelDiameterMeters.

--- robot_code/src/main/java/frc/robot/Constants.java
+++ robot_code/src/main/java/frc/robot/Constants.java
@@ -16,17 +16,16 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
  */
 public final class Constants {
   public static final class DriveConstants {
-    public static final int kLeftMotor1Port = 0;
+    public static final int kLeftMotor1Port = 2;
     public static final int kLeftMotor2Port = 1;
-    public static final int kRightMotor1Port = 2;
-    public static final int kRightMotor2Port = 3;
+    public static final int kRightMotor1Port = 3;
+    public static final int kRightMotor2Port = 4;
 
-    public static final int[] kLeftEncoderPorts = new int[] {0, 1};
-    public static final int[] kRightEncoderPorts = new int[] {2, 3};
+    public static final int[] kLeftEncoderPorts = new int[] {2, 1};
+    public static final int[] kRightEncoderPorts = new int[] {3, 4};
     public static final boolean kLeftEncoderReversed = false;
     public static final boolean kRightEncoderReversed = true;
-
-    public static final double kTrackwidthMeters = 0.69;
+    public static final double kTrackwidthMeters = 1.214821901;
     public static final DifferentialDriveKinematics kDriveKinematics =
         new DifferentialDriveKinematics(kTrackwidthMeters);
 
@@ -41,12 +40,12 @@ public final class Constants {
     // for *your* robot's drive.
     // The Robot Characterization Toolsuite provides a convenient tool for obtaining these
     // values for your robot.
-    public static final double ksVolts = 0.22;
-    public static final double kvVoltSecondsPerMeter = 1.98;
-    public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+    public static final double ksVolts = 0.087;
+    public static final double kvVoltSecondsPerMeter = 2.91;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.303;
 
     // Example value only - as above, this must be tuned for your drive!
-    public static final double kPDriveVel = 8.5;
+    public static final double kPDriveVel = 11.4;
   }
 
   public static final class OIConstants {
@@ -54,8 +53,8 @@ public final class Constants {
   }
 
   public static final class AutoConstants {
-    public static final double kMaxSpeedMetersPerSecond = 3;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static final double kMaxSpeedMetersPerSecond = .5;
+    public static final double kMaxAccelerationMetersPerSecondSquared = .05;
 
     // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
     public static final double kRamseteB = 2;
