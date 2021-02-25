6e1a7776e27ee24420c2d608953565824971bd1d
Jack Holt <jack.g.holt@gmail.com>
2021-02-25 17:35:11 -0600

    Fixes method of sending inc and dec methods for index to Shuffleboard.

--- robocode2/src/main/java/frc/robot/RobotContainer.java
+++ robocode2/src/main/java/frc/robot/RobotContainer.java
@@ -86,8 +86,8 @@ public class RobotContainer {
     
     // Adds the GetAveragedistance command to SmartDashboard
     SmartDashboard.putData(new GetAverageDistance(searchSystem, 3));
-    SmartDashboard.putData(new RunCommand(() -> incIndex()));
-    SmartDashboard.putData(new RunCommand(() -> decIndex()));
+    SmartDashboard.putData("incIndex", new RunCommand(() -> incIndex()));
+    SmartDashboard.putData("decIndex", new RunCommand(() -> decIndex()));
   }
 
   /**
